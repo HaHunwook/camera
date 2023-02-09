bdfa03c3741787c0f5cfef830b8c8b251999e8fa
HaHunwook <gjsdnr15@gmail.com>
2023-02-09 10:12:55 +0900

    camera

---
+++ src/camera/src/cam_pub.cpp
@@ -0,0 +1,15 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  // ...
+}
+int main(int argc,char** argv)
+{
+    ros::init(argc,argv,"turtle_drive");
+    ros::NodeHandle nh;
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber sub = it.subscribe("in_image_base_topic", 1, imageCallback);
+    image_transport::Publisher pub = it.advertise("out_image_base_topic", 1);
+}

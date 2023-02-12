19530e0942e3d1a49687aff847364805ca307d27
HaHunwook <gjsdnr15@gmail.com>
2023-02-12 20:39:35 +0900

    camera

--- src/camera/src/cam_sub.cpp
+++ src/camera/src/cam_sub.cpp
@@ -1,37 +1,3 @@
-// #include <ros/ros.h>
-// #include <image_transport/image_transport.h>
-// #include <cv_bridge/cv_bridge.h>
-// #include <sensor_msgs/Image.h>
-// #include <opencv2/highgui/highgui.hpp>
-
-// class cameraON
-// {
-//     ros::NodeHandle nh;
-//     ros::Subscriber image_sub;
-//     public:
-//         cameraON()
-//         {
-//             image_sub = nh.subscribe("/usb_cam/image_raw", 1, &cameraON::cam_start_Callback, this);
-//         }
-//         ~cameraON()
-//         {
-
-//         }
-//         void cam_start_Callback(const sensor_msgs::ImageConstPtr& msg)
-//         {
-//             cv::imshow("camera", cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image);
-//             cv::waitKey(5);            
-//         }
-// };
-
-// int main(int argc, char** argv)
-// {
-//     ros::init(argc, argv, "cam_sub");
-//     cameraON ic;
-//     ros::spin();
-//     return 0;
-// }
-
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
@@ -60,8 +26,8 @@ class cam
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "image_listener");
-  cam ic;
-  ros::spin();
-  cv::destroyWindow("view");
+    ros::init(argc, argv, "image_listener");
+    cam ic;
+    ros::spin();
+    cv::destroyWindow("view");
 }

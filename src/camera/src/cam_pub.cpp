fa7cb6eb868b929def89f7f716b0fca4388e4b28
HaHunwook <gjsdnr15@gmail.com>
2023-02-12 19:41:12 +0900

    camera

--- src/camera/src/cam_pub.cpp
+++ src/camera/src/cam_pub.cpp
@@ -1,23 +1,23 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/Image.h>
 #include <opencv2/highgui/highgui.hpp>
-#include "std_msgs/String.h"
-#include <sstream>
+#include <cv_bridge/cv_bridge.h>
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "cam_pub");
-    ros::NodeHandle nh;
-    image_transport::ImageTransport it(nh);
-    image_transport::Publisher image_pub;
-    image_pub = it.advertise("/usb_cam/image_raw", 1);
-    sensor_msgs::Image::ConstPtr msg;
-    while(ros::ok())
-    {
-        image_pub.publish(msg);
-        ros::spinOnce();
-    }
-    return 0;
+  ros::init(argc, argv, "image_publisher");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh); //이미지 형태 변환
+  image_transport::Publisher pub = it.advertise("usb_cam/image", 1);
+
+  cv::VideoCapture cap(0);
+  cv::Mat frame;
+  sensor_msgs::ImagePtr msg;
+  while (nh.ok())
+  {
+    cap >> frame;
+    msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+    pub.publish(msg);
+    ros::spinOnce();
+  }
 }
--- src/camera/src/cam_sub.cpp
+++ src/camera/src/cam_sub.cpp
@@ -1,32 +1,67 @@
-#include <ros/ros.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/Image.h>
-#include <opencv2/highgui/highgui.hpp>
+// #include <ros/ros.h>
+// #include <image_transport/image_transport.h>
+// #include <cv_bridge/cv_bridge.h>
+// #include <sensor_msgs/Image.h>
+// #include <opencv2/highgui/highgui.hpp>
 
-class cameraON
+// class cameraON
+// {
+//     ros::NodeHandle nh;
+//     ros::Subscriber image_sub;
+//     public:
+//         cameraON()
+//         {
+//             image_sub = nh.subscribe("/usb_cam/image_raw", 1, &cameraON::cam_start_Callback, this);
+//         }
+//         ~cameraON()
+//         {
+
+//         }
+//         void cam_start_Callback(const sensor_msgs::ImageConstPtr& msg)
+//         {
+//             cv::imshow("camera", cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image);
+//             cv::waitKey(5);            
+//         }
+// };
+
+// int main(int argc, char** argv)
+// {
+//     ros::init(argc, argv, "cam_sub");
+//     cameraON ic;
+//     ros::spin();
+//     return 0;
+// }
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+class cam
 {
     ros::NodeHandle nh;
-    ros::Subscriber image_sub;
+    image_transport::ImageTransport it;
+    image_transport::Subscriber image_sub;
     public:
-        cameraON()
+        cam(): it(nh)
         {
-            image_sub = nh.subscribe("/usb_cam/image_raw", 1, &cameraON::cam_start_Callback, this);
+            image_sub = it.subscribe("usb_cam/image", 1, &cam::imageCallback, this);
         }
-        ~cameraON()
+        ~cam()
         {
 
         }
-        void cam_start_Callback(const sensor_msgs::ImageConstPtr& msg)
+        void imageCallback(const sensor_msgs::ImageConstPtr& msg)
         {
-            cv::imshow("camera", cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image);
+            cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
             cv::waitKey(5);            
         }
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "cam_sub");
-    cameraON ic;
-    ros::spin();
-    return 0;
+  ros::init(argc, argv, "image_listener");
+  cam ic;
+  ros::spin();
+  cv::destroyWindow("view");
 }
